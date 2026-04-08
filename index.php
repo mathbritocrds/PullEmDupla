b6894154997ef2aa8289485d73ed744d524d1ddb
Robert Rodrigues <145489392+rodriguesz9@users.noreply.github.com>
2026-04-08 16:15:53 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+    $owner = "Robert";
+    $contributor = null;
+?>
+
+<h1>Pull request em Dupla</h1>
+
+<?php 
+    echo '<h2 style="color:green"> Dono: ',$owner.'</h2>'
+?>
+
+<h2>Contribuinte: 
+
+<?php
+    echo $contributor;
+    
+?>
+
+</h2>
